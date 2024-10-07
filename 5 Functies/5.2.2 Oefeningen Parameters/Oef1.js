0c39456cb33aa1a46f4661727cc31353c0277502
Sybren Deckers <sybie2007@hotmail.com>
2024-10-07 14:38:22 +0200

    oefening 5.2.2 added

---
+++ 5 Functies/5.2.2 Oefeningen Parameters/Oef1.js
@@ -0,0 +1,5 @@
+function berekenTotaalPrijs(prijs, aantal){
+    let totaal = prijs * aantal;
+    console.log('De totaalprijs is ' + totaal);
+}
+console.log(berekenTotaalPrijs(5, 10));
---
+++ 5 Functies/5.2.2 Oefeningen Parameters/Oef2.js
@@ -0,0 +1,5 @@
+function berekenLengte(naam1, naam2){
+    let nieuwe = naam1.length + naam2.length;
+    console.log('De totale lengte van de namen is ' + nieuwe + ' tekens');
+}               
+berekenLengte("Henk","Steen");
